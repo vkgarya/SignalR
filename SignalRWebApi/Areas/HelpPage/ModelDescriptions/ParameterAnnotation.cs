c4afc8ed228e70eccc4145a687a4c6196dbe15e6
vijay <vijaykg14789@gmail.com>
2021-04-27 11:26:19 +0530

    Add project files.

---
+++ SignalRWebApi/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
